4b6391553f6148939f5cffe267ada632aa9dea63
Salihuk <salihuabubakar08170@gmail.com>
2025-11-24 16:25:22 +0100

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,28 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
     proxy: {
       "/auth": {
-        target: "http://localhost:5001",
+        target: process.env.API_URL || "http://localhost:5001",
         changeOrigin: true
       },
       "/checkin": {
-        target: "http://localhost:5001",
+        target: process.env.API_URL || "http://localhost:5001",
         changeOrigin: true
       },
       "/profile": {
-        target: "http://localhost:5001",
+        target: process.env.API_URL || "http://localhost:5001",
         changeOrigin: true
       },
       "/leaderboard": {
-        target: "http://localhost:5001",
+        target: process.env.API_URL || "http://localhost:5001",
         changeOrigin: true
       }
     }
